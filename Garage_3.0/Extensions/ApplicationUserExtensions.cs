3abcbfbab6541dcf2310e66ea00b79bb0403fd39
Jacob <jackan202@gmail.com>
2026-01-16 16:24:00 +0100

    Extension method to get the users age

--- Garage_3.0/Extensions/ApplicationUserExtensions.cs
+++ Garage_3.0/Extensions/ApplicationUserExtensions.cs
@@ -6,12 +6,13 @@ namespace Garage_3._0.Extensions
     {
         public static int GetAge(this ApplicationUser user)
         {
-            int year = int.Parse(user.PersonalNumber[0..3]);
-            int month = int.Parse(user.PersonalNumber[4..5]);
-            int day = int.Parse(user.PersonalNumber[6..8]);
+            string personalNumber = user.PersonalNumber;
+            int year = int.Parse(personalNumber[..4]);
+            int month = int.Parse(personalNumber[4..6]);
+            int day = int.Parse(personalNumber[6..8]);
             DateTime date = new(year, month, day);
 
-            return (int)((DateTime.Now - date).TotalDays % 365);
+            return (int)((DateTime.Now - date).TotalDays / 365);
         }
     }
 }
