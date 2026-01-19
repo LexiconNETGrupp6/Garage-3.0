f84a69aece9f00958f222f522879e8f19a1c0e69
Jobayerscode <jobayer.b.showkat@gmail.com>
2026-01-19 16:13:11 +0600

    added access control

--- Garage_3.0/Controllers/ParkingSpotsController.cs
+++ Garage_3.0/Controllers/ParkingSpotsController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Garage_3._0.Data;
+using Garage_3._0.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Garage_3._0.Data;
-using Garage_3._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Garage_3._0.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ParkingSpotsController : Controller
     {
         private readonly ApplicationDbContext _context;
